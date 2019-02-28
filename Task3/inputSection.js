4c458decefd52bd7eeb9e420de5aa4fc1653a059
nurkrb <47189237+nurkrb@users.noreply.github.com>
2019-02-28 14:41:11 +0300

    Create inputSection.js

---
+++ Task3/inputSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class InputSection extends React.Component {
+    render(){
+      return (
+        <div className="inputDiv">
+          Hello
+        </div>
+      )
+    }
+  }
+  
+  
+  export default InputSection
